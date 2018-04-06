407de0bde057d4f0a747dc485689b5e7ec950c42
xmontr <xmontr@live.com>
2018-04-06 08:48:15 +0200

    dd

--- eclipsePlugin/src/main/java/tcxmlplugin/composite/ImportScriptComposite.java
+++ eclipsePlugin/src/main/java/tcxmlplugin/composite/ImportScriptComposite.java
@@ -7,6 +7,7 @@ public class ImportScriptComposite extends Composite{
 	public ImportScriptComposite(Composite parent, int style) {
 		super(parent, style);
 		// TODO Auto-generated constructor stub
+	
 		
 		
 	}
