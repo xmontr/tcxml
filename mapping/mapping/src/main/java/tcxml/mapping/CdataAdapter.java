7cd2e5867bee137391b66400bcdfc77248f48b1e
Xavier <Xavier@DESKTOP-DTUP5S8>
2020-05-01 14:06:39 +0200

    support for CDATA marshalling
    add annotation in tcxml.xsd to support CDATA section for ident when
    marshalling.

---
+++ mapping/mapping/src/main/java/tcxml/mapping/CdataAdapter.java
@@ -0,0 +1,19 @@
+package tcxml.mapping;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class CdataAdapter extends XmlAdapter<String, String> {
+	
+	
+    @Override
+    public String marshal(String arg0) throws Exception {
+        return "<![CDATA[" + arg0 + "]]>";
+    }
+    @Override
+    public String unmarshal(String arg0) throws Exception {
+        return arg0;
+    }
+	
+	
+
+}
